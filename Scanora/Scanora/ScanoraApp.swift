35dcf8f534d14d770aa1acae4351a51e9aa9c841
White Dxke <whitedxke@gmail.com>
2026-02-23 20:50:21 +0300

    Created a new empty project.

---
+++ Scanora/Scanora/ScanoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScanoraApp.swift
+//  Scanora
+//
+//  Created by Pavel Betenya on 23.02.26.
+//
+
+import SwiftUI
+
+@main
+struct ScanoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
